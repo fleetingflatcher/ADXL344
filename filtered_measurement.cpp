bc4075a4ef70f23fbd8e5dfba5a3323b0900b3a7
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2023-04-18 12:20:30 -0500

    Major Update FilteredMeasurement
    Implemented dead-simple moving average where window size n=2.

--- filtered_measurement.cpp
+++ filtered_measurement.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "filtered_measurement.h"
+
+FilteredMeasurement::FilteredMeasurement() {}
+
+FilteredMeasurement::FilteredMeasurement(RawMeasurement next, FilteredMeasurement prev) {
+	X_value = (next.X() + prev.X()) / 2;
+	Y_value = (next.Y() + prev.Y()) / 2;
+	Z_value = (next.Z() + prev.Z()) / 2;
+	pitch_value = (next.pitch() + prev.pitch()) / 2;
+	roll_value = (next.roll() + prev.roll()) / 2;
+}
--- filtered_measurement.h
+++ filtered_measurement.h
@@ -5,9 +5,25 @@
 #ifndef ADXL344_FILTERED_MEASUREMENT_H
 #define ADXL344_FILTERED_MEASUREMENT_H
 
+#include "raw_measurement.h"
 
-class filtered_measurement {
+class FilteredMeasurement {
+public:
+	FilteredMeasurement();
+    FilteredMeasurement(RawMeasurement next, FilteredMeasurement prev);
 
+    const int16_t X()       { return X_value; }
+    const int16_t Y()       { return Y_value; }
+    const int16_t Z()       { return Z_value; }
+    const float pitch()   { return pitch_value; }   // Theta
+    const float roll()    { return roll_value; }    // Phi
+
+private:
+    int16_t X_value;
+    int16_t Y_value;
+    int16_t Z_value;
+    float pitch_value;
+    float roll_value;
 };
 
 
