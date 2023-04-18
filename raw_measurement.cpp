dd82059cb5a8d747e94bb201da8cd315b6a4f91c
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2023-04-18 12:21:52 -0500

    Major Update RawMeasurement
    WIP here. Pitch calculation is written, but gives odd results. Orientation register in the ADXL may solve this problem for us.

--- raw_measurement.cpp
+++ raw_measurement.cpp
@@ -4,37 +4,17 @@
 #include "raw_measurement.h"
 #include <cmath>
 
-raw_measurement::raw_measurement(int16 X, int16 Y, int16 Z) {
+RawMeasurement::RawMeasurement(int16_t X, int16_t Y, int16_t Z) {
     X_value = X;
     Y_value = Y;
     Z_value = Z;
+    pitch_value = calculatePitch();
+    roll_value = calculateRoll();
 }
 
-// TODO: Short version: For all the following functions, _read needs to be checked.
-// Summary: check _read for each function.
-// If it has been read already throw an out-of-date flag somewhere
-// Still return the old value though.
 
-int16 raw_measurement::X()
-{
-    return X_value;
+float RawMeasurement::calculateRoll() { return 0; }
+float RawMeasurement::calculatePitch() {
+    return atan((Y_value/Z_value) * (1 / sqrt(pow(Y_value, 2) + pow(Z_value, 2))));
+	//return atan(-1 * X_out / sqrt(pow(Y_out, 2) + pow(Z_out, 2))) * 180 / M_PI;
 }
-int16 raw_measurement::Y()
-{
-    return Y_value;
-}
-int16 raw_measurement::Z()
-{
-    return Z_value;
-}
-// TODO: Check these calculations & rename variables; complete the other TODO expanding the constructor to include previous orientation values
-/*
-float raw_measurement::calculateRoll() {
-    return atan(Y_out / sqrt(pow(X_out, 2) + pow(Z_out, 2))) * 180 / M_PI;
-    rollF = 0.94 * rollF + 0.06 * roll;
-}
-float raw_measurement::calculatePitch() {
-    return atan(-1 * X_out / sqrt(pow(Y_out, 2) + pow(Z_out, 2))) * 180 / M_PI;
-    pitchF = 0.94 * pitchF + 0.06 * pitch;
-}
-*/
--- raw_measurement.h
+++ raw_measurement.h
@@ -5,35 +5,28 @@
 #ifndef ADXL344_RAW_MEASUREMENT_H
 #define ADXL344_RAW_MEASUREMENT_H
 
-typedef short int int16;
+#include <stdint.h>
 
-class raw_measurement {
-    // TODO: Implement roll, pitch fields and calculations. Constructor can handle all of this.
+class RawMeasurement {
 public:
-    // TODO: Change signature to include two floats, previous roll & pitch for implementation of IIR low-pass filter
-    raw_measurement(int16 X, int16 Y, int16 Z);
+	RawMeasurement();
+    RawMeasurement(int16_t X, int16_t Y, int16_t Z);
 
-    int16 X() ;
-    int16 Y() ;
-    int16 Z() ;
-    int16 T() ;
-    int16 P() ;
+    const int16_t X()       { return X_value; }
+    const int16_t Y()       { return Y_value; }
+    const int16_t Z()       { return Z_value; }
+    const float pitch()   { return pitch_value; }   // Theta
+    const float roll()    { return roll_value; }    // Phi
 
 private:
-    int16 X_value;
-    int16 Y_value;
-    int16 Z_value;
+    int16_t X_value;
+    int16_t Y_value;
+    int16_t Z_value;
+    float pitch_value;
+    float roll_value;
 
-    int16 theta_value;
-    int16 phi_value;
-
-    bool X_read;    // These variables will be set to true if the axis of the measurement is "old"
-    bool Y_read;    // eg. Accelerometer::takeNewReading() sets all to false
-    bool Z_read;    // eg. Accelerometer::readZ() sets z_read to true
-    bool T_read;
-    bool P_read;
-
-    float calculate_roll();
+	float calculateRoll();
+	float calculatePitch() ;
 };
 
 #endif //ADXL344_RAW_MEASUREMENT_H
