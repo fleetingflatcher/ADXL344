982daefd9d2f621298d067ab427672b0dede0e04
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2023-04-18 12:18:42 -0500

    Major Update ADXL344
    Enums added to header file to indicate a few named states, standard data rates and FIFO modes.
    
    setup() -> init()
    
    Added configuration registers to setup here. More work is necessary to determine the appropriate configuration, and to distribute these register accesses to individual functions.
    
    takeNewMeasurement() -> read()
    flushFilter()
    
    Resets the filtered measurement.
    
    zeroOut()
    
    Implemented, but broken. Unit testing recommended.
    
    configSet_X ()
    
    many of these implemented, more are necessary.

--- ADXL344.cpp
+++ ADXL344.cpp
@@ -4,61 +4,160 @@
 
 #include "ADXL344.h"
 
+ADXL344::ADXL344() {}
+
 // TODO: read through this; make sure it makes sense.
 // TODO: More than that, make sure there are no config registers we're missing.
-void ADXL344::setup (int8_t X_Offset, int8) {
+void ADXL344::init() {
     Wire.begin(); // Initiate the Wire library
-    // Set adxl344 in measuring mode
-    Wire.beginTransmission(address); // Start communicating with the device
-    Wire.write(0x2D); // Access/ talk to POWER_CTL Register - 0x2D
-    // Enable measurement
-    Wire.write(8); // Bit D3 High for measuring enable (8dec -> 0000 1000 binary)
-    Wire.endTransmission();
-    delay(10);
 
-    //Off-set Calibration
-    //X-axis
-    Wire.beginTransmission(address);
-    Wire.write(0x1E);
-    Wire.write(1);
+    // Start measuring. Set link bit.
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x2D); 
+    Wire.write(0x28);
     Wire.endTransmission();
     delay(10);
-    //Y-axis
-    Wire.beginTransmission(address);
-    Wire.write(0x1F);
-    Wire.write(-2);
+    
+    // Set up activity/inactivity monitoring
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x27);
+    Wire.write(0x77);
     Wire.endTransmission();
-    delay(10);
 
-    //Z-axis
-    Wire.beginTransmission(address);
-    Wire.write(0x20);
-    Wire.write(-9);
+    // Set activity threshold
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x24);
+    Wire.write(0xFF);
     Wire.endTransmission();
-    delay(10);
+
+    // Set inactivity threshold
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x25);
+    Wire.write(0x01);
+    Wire.endTransmission();
+
+    // Set activity timeout
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x26);
+    Wire.write(0x01);
+    Wire.endTransmission();
+
+    // Enable interrupts
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x2E);
+    Wire.write(0x98);
+    Wire.endTransmission();
+
+	//set2gResolution();
 }
-// TODO: check the logic here
-void ADXL344::takeNewMeasurement() {
-    Wire.beginTransmission(address);
-    Wire.write(0x32); // Start with register 0x32 (ACCEL_XOUT_H)
+
+void ADXL344::configSet_manualCalibration() {
+	configSet_Offsets(-10, 80, -100);
+}
+
+void ADXL344::flushFilter() { measurement = FilteredMeasurement(); }
+
+void ADXL344::zeroOut(int numSamples) {
+	//int rate = configGet_DataRate();
+	int rate = 100;
+
+	int x;
+	int y;
+	int z;
+
+	for (int i = 0; i < numSamples; ++i) {
+		read();
+		x -= get().X();
+		y -= get().Y();
+		z -= get().Z();
+		// Delay for 1000/rate, time in ms between samples
+		delay((int) (1000 / (float)rate)); 
+	}
+	x /= numSamples;
+	y /= numSamples;
+	z /= numSamples;
+
+	configSet_Offsets(x, y, z);
+}
+
+void ADXL344::read() {
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x32);
     Wire.endTransmission(false);
-    Wire.requestFrom(address, 6, true);
+    Wire.requestFrom(ADDRESS, 6, true);
 
 
-    int16 X_out = ( Wire.read() | Wire.read() << 8); // X-axis value
-    int16 Y_out = ( Wire.read() | Wire.read() << 8); // Y-axis value
-    int16 Z_out = ( Wire.read() | Wire.read() << 8); // Z-axis value
+    int16_t x = ( Wire.read() | Wire.read() << 8); // X-axis value
+    int16_t y = ( Wire.read() | Wire.read() << 8); // Y-axis value
+    int16_t z = ( Wire.read() | Wire.read() << 8); // Z-axis value
 
-    //Measurement nextMeasurement(X_out, Y_out, Z_out, , measurement.pitch);
-
-//For a range of +-2g, we need to divide the raw values by 256, according to the datasheet
-    X_out = X_out / rangeDivider;
-    Y_out = Y_out / rangeDivider;
-    Z_out = Z_out / rangeDivider;
-
-    measurement = nextMeasurement;
+    RawMeasurement raw = RawMeasurement(x, y, z);
+	
+	measurement = FilteredMeasurement(raw, measurement);
 }
-// TODO: Implement range setting function. Default is 2g
-void ADXL344::setRange() {
-    return;
+
+void ADXL344::configSet_2gResolution() {
+	Wire.beginTransmission(ADDRESS);
+	Wire.write(0x31);
+	Wire.requestFrom(ADDRESS, 1, true);
+	uint8_t DAT_FMT_REG = Wire.read();
+	Wire.endTransmission();
+
+	DAT_FMT_REG &= ~0x0B;
+
+	Wire.beginTransmission(ADDRESS);
+	Wire.write(0x31);
+	Wire.write(DAT_FMT_REG);
+	Wire.endTransmission();
+	delay(5);
+}
+
+void ADXL344::configSet_DataRate(int4_t freq) {
+
+}
+
+void ADXL344::configSet_FIFOMode(ADXL344_FIFO_MODE mode) {
+  Wire.beginTransmission(ADDRESS);
+  Wire.write(0x38);
+  Wire.write(mode);
+  Wire.endTransmission();
+  delay(5);
+}
+
+void ADXL344::configSet_Offsets(int8_t x, int8_t y, int8_t z) {
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x1E);
+    Wire.write(x);
+    Wire.endTransmission();
+	Wire.beginTransmission(ADDRESS);
+    Wire.write(0x1F);
+    Wire.write(y);
+    Wire.endTransmission();
+	Wire.beginTransmission(ADDRESS);
+    Wire.write(0x20);
+    Wire.write(z);
+    Wire.endTransmission();
+    delay(5);
+}
+
+void ADXL344::configSet_OffsetX(int8_t offset) {
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x1E);
+    Wire.write(offset);
+    Wire.endTransmission();
+    delay(5);
+}
+void ADXL344::configSet_OffsetY(int8_t offset) {
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x1F);
+    Wire.write(offset);
+    Wire.endTransmission();
+    delay(5);
+}
+void ADXL344::configSet_OffsetZ(int8_t offset) {
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(0x20);
+    Wire.write(offset);
+    Wire.endTransmission();
+    delay(5);
 }
--- ADXL344.h
+++ ADXL344.h
@@ -2,30 +2,63 @@
 // Created by corwin on 4/5/2023.
 //
 
-#include <stdint.h>
+typedef struct {
+	unsigned int value : 4;
+} int4_t;
 
-#include <Arduino.h>
+#include <stdint.h>
 #include <Wire.h>
+#include <Arduino.h>
 
 #include "raw_measurement.h"
+#include "filtered_measurement.h"
 
 #ifndef ADXL344_H
 #define ADXL344_H
 
+enum ADXL344_DATA_RATE {
+    ADXL_FL_VSLOW = 0x05, // Corresponds to 3.13 Hz
+    ADXL_FL_SLOW  = 0x07, // Corresponds to 12.5 Hz
+    ADXL_FL_FAST  = 0x09,   // Corresponds to 50.0 Hz
+	ADXL_FL_DEFAULT = 0x0A // Corresponds to 100.0 Hz
+};
+enum ADXL344_FIFO_MODE {
+  ADXL344_FIFO_BYPASS = 0,
+  ADXL344_FIFO_FIFO,
+  ADXL344_FIFO_STREAM,
+  ADXL344_FIFO_TRIGGER  
+};
+
 class ADXL344 {
 public :
-    void setup() ;
-    void setRange() ;
-    void changeOffset();
-    void changeOffsetX();
-    void changeOffsetY();
-    void changeOffsetZ();
-    void takeNewMeasurement();
-    void resetMeasurement();
+	ADXL344();
+    void init() ;
+
+	ADXL344_DATA_RATE configGet_DataRate();
+	int8_t configGet_OffsetX();
+	int8_t configGet_OffsetY();
+	int8_t configGet_OffsetZ();
+
+	void configSet_manualCalibration();
+	void configSet_Offsets(int8_t x, int8_t y, int8_t z);
+    void configSet_OffsetX(int8_t x);
+    void configSet_OffsetY(int8_t y);
+    void configSet_OffsetZ(int8_t z);
+	void configSet_DataRate(int4_t freq);
+	void configSet_FIFOMode(ADXL344_FIFO_MODE);
+	void configSet_2gResolution();
+    
+	FilteredMeasurement get() { return measurement; }
+	void read();
+    void flushFilter();
+	void zeroOut(int numSamples);
 
 private:
+    FilteredMeasurement measurement;
     int rangeDivider = 256;
-    const int address = 0x53; // The adxl344 sensor I^2C address
+    const int ADDRESS = 0x53; // The adxl344 sensor I^2C address
+
+	
 };
 
 #endif //ADXL344_H
